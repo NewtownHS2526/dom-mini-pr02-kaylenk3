73ad756cbcaac8651420be5febf7251455fad3cf
Kayleen Kurnia <kayleenk3@nycstudents.net>
2026-01-09 11:47:10 -0500

    donee

--- script.js
+++ script.js
@@ -7,31 +7,40 @@ let nebula_spot = 1;
 let comet_spot  = 1;
 
 // Query Selectors
-const apolloBtn   = 
-const nebulaBtn   = 
-const cometBtn    = 
+const apolloBtn   = document.querySelector("#apollo-btn");
+const nebulaBtn   = document.querySelector("#nebula-btn");
+const cometBtn    = document.querySelector("#comet-btn");
 
-const apolloRocket = 
-const nebulaRocket = 
-const cometRocket  = 
+const apolloRocket = document.querySelector("#apollo-rocket");
+const nebulaRocket = document.querySelector("#nebula-rocket");
+const cometRocket  = document.querySelector("#comet-rocket");
 
-const winnerBox = 
+const winnerBox = document.querySelector("#winner");
 
 // Click Handlers
-const boostApollo = () => {
-  
+const boostApollo = (e) => {
+  apollo_spot += 2;
+  changePosition(apolloRocket, apollo_spot);
 };
 
-const boostNebula = () => {
-
+const boostNebula = (e) => {
+  nebula_spot += 2;
+  changePosition(nebulaRocket, nebula_spot);
 };
 
-const boostComet = () => {
-
+const boostComet = (e) => {
+  comet_spot += 2;
+  changePosition(cometRocket, comet_spot);
 };
 
 // Winner Check
-
+const checkWinner = (spot, item) => {
+  if (spot === 6) {
+  winnerBox.innerHTML = item + `is THE WINNER!!!`
+  }
+};
 
 // Events
-
+apolloBtn.addEventListener("click", boostApollo);
+nebulaBtn.addEventListener("click", boostNebula);
+cometBtn.addEventListener("click", boostComet);
